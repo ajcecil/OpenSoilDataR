d75fe6eff4939e30444f896a023d1692db049567
Meyer P Bohn <45853601+MollicMeyer@users.noreply.github.com>
2025-03-21 13:41:53 -0500

    Add files via upload

--- R/s.zonalstats.R
+++ R/s.zonalstats.R
@@ -4,7 +4,7 @@
 #' SoilGrids v2 (SG2), SOLUS100 (SOL), and CSRL Soil props 800. It ensures that depth intervals from different
 #' datasets are consistently translated, and supports depth-weighted averaging.
 #'
-#' @param soil_data A `SpatRaster` or list of `SpatRaster` objects from `fetch_PSP`, `fetch_SG2`, `fetch_SOL`, or `fetch_CSRL`.
+#' @param soil_data A `SpatRaster` or list of `SpatRaster` objects from `fetch_PSP`, `fetch_SG2`, or `fetch_SOL`.
 #' @param tdepth Numeric. Top depth of interest (cm).
 #' @param bdepth Numeric. Bottom depth of interest (cm).
 #' @param props Character vector. Standardized soil properties (e.g., `"clay"`, `"sand"`, `"bd"`).
@@ -15,45 +15,99 @@
 #' @param MakePlot Logical. If `TRUE`, generates raster plots for each statistic.
 #' @param output_dir Character. Directory to save output files. Set to `NULL` to disable file saving.
 #' 
+#' #' @section Property Lookup:
+#' **Standardized soil properties across datasets.**
+#' 
+#' \preformatted{
+#' property_lookup <- list(
+#' # Water Retention
+#' "wtenthbar"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wtenthbar_r"),
+#' "wthirdbar"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wthirdbar_r"),
+#' "wfifteenbar"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wfifteenbar_r"),
+#' "awc"            = c(PSP = NA, SG2 = NA, SOL = "awc_r", CSRL = NA, SGO = "awc_r"),
+#' 
+#' # Phosphorus
+#' "pbray1"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "pbray1_r"),
+#' "ptotal"         = c(PSP = NA, SG2 = NA, SOL = "ptotal_r", CSRL = NA, SGO = "ptotal_r"),
+#' "ph2osoluble"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ph2osoluble_r"),
+#' "poxalate"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "poxalate_r"),
+#' 
+#' # Bulk Density
+#' "bd"             = c(PSP = "bd_mean", SG2 = "bdod", SOL = "dbovendry_r", CSRL = "bulk_density", SGO = "dbovendry_r"),
+#' "bd_tenthbar"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "dbtenthbar_r"),
+#' "bd_thirdbar"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "dbthirdbar_r"),
+#' "bd_fifteenbar"  = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "dbfifteenbar_r"),
+#' 
+#' # Texture
+#' "clay"           = c(PSP = "clay_mean", SG2 = "clay", SOL = "claytotal", CSRL = "clay_profile", SGO = "claytotal_r"),
+#' "sand"           = c(PSP = "sand_mean", SG2 = "sand", SOL = "sandtotal", CSRL = "sand_profile", SGO = "sandtotal_r"),
+#' "silt"           = c(PSP = "silt_mean", SG2 = "silt", SOL = "silttotal", CSRL = "silt_profile", SGO = "silttotal_r"),
+#' "sandco"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandco_r"),
+#' "sandfine"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandfine_r"),
+#' "sandmed"        = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandmed_r"),
+#' "sandvc"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandvc_r"),
+#' "sandvf"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandvf_r"),
+#' "siltco"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "siltco_r"),
+#' "siltfine"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "siltfine_r"),
+#' 
+#' # Organic Carbon
+#' "som"            = c(PSP = "om_mean", SG2 = NA, SOL = NA, CSRL = "som_max", SGO = "om_r"),
+#' "soc"            = c(PSP = "soc", SG2 = "soc", SOL = "soc", CSRL = "soc_max", SGO = "oc_r"),
+#' "soc_stock"      = c(PSP = NA, SG2 = "ocs", SOL = NA, CSRL = "soc_stock", SGO = NA),
+#' 
+#' # Chemistry
+#' "ph"             = c(PSP = "ph_mean", SG2 = "phh2o", SOL = "ph1to1h2o", CSRL = "ph_profile", SGO = "ph1to1h2o_r"),
+#' "ph_cacl2"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ph01mcacl2_r"),
+#' "ph_oxidized"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "phoxidized_r"),
+#' "cec"            = c(PSP = NA, SG2 = "cec", SOL = "cec7", CSRL = "cec_profile", SGO = "cec7_r"),
+#' "ecec"           = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ecec_r"),
+#' "ec"             = c(PSP = NA, SG2 = NA, SOL = "ec", CSRL = "ec_profile", SGO = "ec_r"),
+#' "ec15"           = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ec15_r"),
+#' "esp"            = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "esp_r"),
+#' "sar"            = c(PSP = NA, SG2 = NA, SOL = "sar", CSRL = "sar", SGO = "sar_r"),
+#' "gypsum"         = c(PSP = NA, SG2 = NA, SOL = "gypsum", CSRL = NA, SGO = "gypsum_r"),
+#' "caco3"          = c(PSP = NA, SG2 = NA, SOL = "caco3", CSRL = "caco3", SGO = "caco3_r"),
+#' 
+#' # Extractables
+#' "extracid"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "extracid_r"),
+#' "extral"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "extral_r"),
+#' "aloxalate"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "aloxalate_r"),
+#' "feoxalate"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "feoxalate_r"),
+#' 
+#' # Mechanics
+#' "lep"            = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "lep_r"),
+#' "ll"             = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ll_r"),
+#' "pi"             = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "pi_r"),
+#' 
+#' # Rock Fragments
+#' "frag3to10"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "frag3to10_r"),
+#' "fraggt10"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "fraggt10_r"),
+#' 
+#' # Fiber
+#' "fiberrubbedpct" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "fiberrubbedpct_r"),
+#' "fiberunrubbedpct" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "fiberunrubbedpct_r"),
+#' 
+#' # Sieve
+#' "sieveno10"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno10_r"),
+#' "sieveno200"     = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno200_r"),
+#' "sieveno4"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno4_r"),
+#' "sieveno40"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno40_r"),
+#' 
+#' # Hydrology
+#' "ksat"           = c(PSP = "ksat_mean", SG2 = NA, SOL = NA, CSRL = "ksat_max", SGO = "ksat_r"),
+#' "wsatiated"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wsatiated_r"),
+
+#' # Erosion
+#' "kw"             = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kwfact"),
+#' "kffact"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kffact"),
+#' "kifact"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kifact"),
+#' "krfact"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "krfact")
+#' )
+#' }
+#' 
 #' @return A list containing:
 #'   - `$Unweighted`: Data frame of zonal statistics per depth interval.
 #'   - `$Weighted`: Data frame of depth-weighted means (if `wtd.mean = TRUE`).
-#'
-#' @section Property Lookup:
-#' **Standardized soil properties across datasets.**
-#'
-#' \preformatted{
-#' property_lookup <- list(
-#' # Bulk Density
-#' "bd" = c(PSP = "bd_mean", SG2 = "bdod", SOL = "dbovendry", CSRL = "bulk_density"),
-#' # Soil Texture (Sand, Silt, Clay)
-#' "clay" = c(PSP = "clay_mean", SG2 = "clay", SOL = "claytotal", CSRL = "clay_profile"),
-#' "sand" = c(PSP = "sand_mean", SG2 = "sand", SOL = "sandtotal", CSRL = "sand_profile"),
-#' "silt" = c(PSP = "silt_mean", SG2 = "silt", SOL = "silttotal", CSRL = "silt_profile"),
-#' # Soil Organic Matter / Carbon
-#' "som" = c(PSP = "om_mean", SG2 = NA, SOL = NA, CSRL = "som_max"),
-#' "soc" = c(PSP = "soc", SG2 = "soc", SOL = "soc_max", CSRL = "soc_max"),
-#' "soc_stock" = c(PSP = NA, SG2 = "ocs", SOL = NA, CSRL = "soc_stock"),
-#' "som_stock" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "som_stock"),
-#pH & Chemistry
-#' "ph" = c(PSP = "ph_mean", SG2 = "phh2o", SOL = "ph1to1h2o", CSRL = "ph_profile"),
-#' "cec" = c(PSP = NA, SG2 = "cec", SOL = "cec7", CSRL = "cec_profile"),
-#' "ec" = c(PSP = NA, SG2 = NA, SOL = "ec", CSRL = "ec_profile"),
-#' "sar" = c(PSP = NA, SG2 = NA, SOL = "sar", CSRL = "sar"),
-#' "caco3" = c(PSP = NA, SG2 = NA, SOL = "caco3", CSRL = "caco3"),
-#' "gypsum" = c(PSP = NA, SG2 = NA, SOL = "gypsum", CSRL = NA),
-#' # Hydrology & Conductivity
-#' "ksat" = c(PSP = "ksat_mean", SG2 = NA, SOL = NA, CSRL = "ksat_max"),
-#' "theta_s" = c(PSP = "theta_s_mean", SG2 = NA, SOL = NA, CSRL = NA),
-#' "theta_r" = c(PSP = "theta_r_mean", SG2 = NA, SOL = NA, CSRL = NA),
-#' # Coarse Fragments & Rock Volume
-#' "coarse_fragments" = c(PSP = NA, SG2 = "cfvo", SOL = "fragvol", CSRL = "rf_025"),
-# Land Use & Erosion
-#' "wind_erodibility_group" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "wind_erodibility_group"),
-#' "wind_erodibility_index" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "wind_erodibility_index"),
-#' "soil_order" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "soil_order"),
-#' "soil_temp_regime" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "soil_temp_regime")
-#' }
 #'
 #' @import terra sf dplyr tidyr
 #' @export
@@ -101,45 +155,91 @@ s.zonalstats <- function(soil_data, tdepth = 0, bdepth = 20,
   }
 
 
-  # **Property Lookup Table** - Standardizing soil properties across datasets
+  # Property Lookup Table for use in s.zonalstats()
   property_lookup <- list(
+    # Water Retention
+    "wtenthbar"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wtenthbar_r"),
+    "wthirdbar"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wthirdbar_r"),
+    "wfifteenbar"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wfifteenbar_r"),
+    "awc"            = c(PSP = NA, SG2 = NA, SOL = "awc_r", CSRL = NA, SGO = "awc_r"),
+    
+    # Phosphorus
+    "pbray1"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "pbray1_r"),
+    "ptotal"         = c(PSP = NA, SG2 = NA, SOL = "ptotal_r", CSRL = NA, SGO = "ptotal_r"),
+    "ph2osoluble"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ph2osoluble_r"),
+    "poxalate"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "poxalate_r"),
+    
     # Bulk Density
-    "bd" = c(PSP = "bd_mean", SG2 = "bdod", SOL = "dbovendry", CSRL = "bulk_density"),
+    "bd"             = c(PSP = "bd_mean", SG2 = "bdod", SOL = "dbovendry_r", CSRL = "bulk_density", SGO = "dbovendry_r"),
+    "bd_tenthbar"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "dbtenthbar_r"),
+    "bd_thirdbar"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "dbthirdbar_r"),
+    "bd_fifteenbar"  = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "dbfifteenbar_r"),
     
-    # Soil Texture (Sand, Silt, Clay)
-    "clay" = c(PSP = "clay_mean", SG2 = "clay", SOL = "claytotal", CSRL = "clay_profile"),
-    "sand" = c(PSP = "sand_mean", SG2 = "sand", SOL = "sandtotal", CSRL = "sand_profile"),
-    "silt" = c(PSP = "silt_mean", SG2 = "silt", SOL = "silttotal", CSRL = "silt_profile"),
+    # Texture
+    "clay"           = c(PSP = "clay_mean", SG2 = "clay", SOL = "claytotal", CSRL = "clay_profile", SGO = "claytotal_r"),
+    "sand"           = c(PSP = "sand_mean", SG2 = "sand", SOL = "sandtotal", CSRL = "sand_profile", SGO = "sandtotal_r"),
+    "silt"           = c(PSP = "silt_mean", SG2 = "silt", SOL = "silttotal", CSRL = "silt_profile", SGO = "silttotal_r"),
+    "sandco"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandco_r"),
+    "sandfine"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandfine_r"),
+    "sandmed"        = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandmed_r"),
+    "sandvc"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandvc_r"),
+    "sandvf"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandvf_r"),
+    "siltco"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "siltco_r"),
+    "siltfine"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "siltfine_r"),
     
-    # Soil Organic Matter / Carbon
-    "som" = c(PSP = "om_mean", SG2 = NA, SOL = NA, CSRL = "som_max"),
-    "soc" = c(PSP = "soc", SG2 = "soc", SOL = "soc", CSRL = "soc_max"),
-    "soc_stock" = c(PSP = NA, SG2 = "ocs", SOL = NA, CSRL = "soc_stock"),
-    "som_stock" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "som_stock"),
+    # Organic Carbon
+    "som"            = c(PSP = "om_mean", SG2 = NA, SOL = NA, CSRL = "som_max", SGO = "om_r"),
+    "soc"            = c(PSP = "soc", SG2 = "soc", SOL = "soc", CSRL = "soc_max", SGO = "oc_r"),
+    "soc_stock"      = c(PSP = NA, SG2 = "ocs", SOL = NA, CSRL = "soc_stock", SGO = NA),
     
-    #pH & Chemistry
-    "ph" = c(PSP = "ph_mean", SG2 = "phh2o", SOL = "ph1to1h2o", CSRL = "ph_profile"),
-    "cec" = c(PSP = NA, SG2 = "cec", SOL = "cec7", CSRL = "cec_profile"),
-    "ec" = c(PSP = NA, SG2 = NA, SOL = "ec", CSRL = "ec_profile"),
-    "sar" = c(PSP = NA, SG2 = NA, SOL = "sar", CSRL = "sar"),
-    "caco3" = c(PSP = NA, SG2 = NA, SOL = "caco3", CSRL = "caco3"),
-    "gypsum" = c(PSP = NA, SG2 = NA, SOL = "gypsum", CSRL = NA),
+    # Chemistry
+    "ph"             = c(PSP = "ph_mean", SG2 = "phh2o", SOL = "ph1to1h2o", CSRL = "ph_profile", SGO = "ph1to1h2o_r"),
+    "ph_cacl2"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ph01mcacl2_r"),
+    "ph_oxidized"    = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "phoxidized_r"),
+    "cec"            = c(PSP = NA, SG2 = "cec", SOL = "cec7", CSRL = "cec_profile", SGO = "cec7_r"),
+    "ecec"           = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ecec_r"),
+    "ec"             = c(PSP = NA, SG2 = NA, SOL = "ec", CSRL = "ec_profile", SGO = "ec_r"),
+    "ec15"           = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ec15_r"),
+    "esp"            = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "esp_r"),
+    "sar"            = c(PSP = NA, SG2 = NA, SOL = "sar", CSRL = "sar", SGO = "sar_r"),
+    "gypsum"         = c(PSP = NA, SG2 = NA, SOL = "gypsum", CSRL = NA, SGO = "gypsum_r"),
+    "caco3"          = c(PSP = NA, SG2 = NA, SOL = "caco3", CSRL = "caco3", SGO = "caco3_r"),
     
-    # Hydrology & Conductivity
-    "ksat" = c(PSP = "ksat_mean", SG2 = NA, SOL = NA, CSRL = "ksat_max"),
-    "theta_s" = c(PSP = "theta_s_mean", SG2 = NA, SOL = NA, CSRL = NA),
-    "theta_r" = c(PSP = "theta_r_mean", SG2 = NA, SOL = NA, CSRL = NA),
+    # Extractables
+    "extracid"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "extracid_r"),
+    "extral"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "extral_r"),
+    "aloxalate"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "aloxalate_r"),
+    "feoxalate"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "feoxalate_r"),
     
-    # Coarse Fragments & Rock Volume
-    "coarse_fragments" = c(PSP = NA, SG2 = "cfvo", SOL = "fragvol", CSRL = "rf_025"),
+    # Mechanics
+    "lep"            = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "lep_r"),
+    "ll"             = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ll_r"),
+    "pi"             = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "pi_r"),
     
-    # Land Use & Erosion
-    "wind_erodibility_group" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "wind_erodibility_group"),
-    "wind_erodibility_index" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "wind_erodibility_index"),
-    "soil_order" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "soil_order"),
-    "soil_temp_regime" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = "soil_temp_regime")
+    # Rock Fragments
+    "frag3to10"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "frag3to10_r"),
+    "fraggt10"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "fraggt10_r"),
+    
+    # Fiber
+    "fiberrubbedpct" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "fiberrubbedpct_r"),
+    "fiberunrubbedpct" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "fiberunrubbedpct_r"),
+    
+    # Sieve
+    "sieveno10"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno10_r"),
+    "sieveno200"     = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno200_r"),
+    "sieveno4"       = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno4_r"),
+    "sieveno40"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno40_r"),
+    
+    # Hydrology
+    "ksat"           = c(PSP = "ksat_mean", SG2 = NA, SOL = NA, CSRL = "ksat_max", SGO = "ksat_r"),
+    "wsatiated"      = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "wsatiated_r"),
+    
+    # Erosion
+    "kw"             = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kwfact"),
+    "kffact"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kffact"),
+    "kifact"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kifact"),
+    "krfact"         = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "krfact")
   )
-  
 
   # **Depth Interval Lookup Table** - Standardizing depth labels across datasets
   depth_interval_lookup <- list(
